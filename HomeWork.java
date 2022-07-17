76bda09ed0c9047ebd8994fd3d33c8756b855699
Vladimir Yelkin <vl.yelkin@gmail.com>
2022-07-17 21:48:24 +0400

    Update HomeWork.java
    Formated Source code

--- HomeWork.java
+++ HomeWork.java
@@ -1,5 +1,6 @@
 import java.util.Base64;
 import java.util.Scanner;
+
 // Зарегистрироваться на github.com
 // Установить git
 // Установить JDK и среду разработки
@@ -11,11 +12,11 @@ import java.util.Scanner;
 // Сделайте pull-request с программой.
 // Отправить в чат задания ссылку на pull-request
 public class HomeWork {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-System.out.println("Please enter you full name");
-String fullName = sc.nextLine();
-String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
-System.out.println("Program result: " + encodedFullName);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter you full name");
+        String fullName = sc.nextLine();
+        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        System.out.println("Program result: " + encodedFullName);
+    }
 }
